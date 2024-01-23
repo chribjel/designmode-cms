94610f9eeb576f1786aa804c9f54cbf4ca56b8c6
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2024-01-23 12:01:50 +0100

    Add environment variables for KV REST API configuration

--- src/env.mjs
+++ src/env.mjs
@@ -8,6 +8,10 @@ export const env = createEnv({
 	 */
 	server: {
 		NODE_ENV: z.enum(["development", "test", "production"]),
+		KV_REST_API_READ_ONLY_TOKEN: z.string().min(1),
+		KV_REST_API_TOKEN: z.string().min(1),
+		KV_REST_API_URL: z.string().url(),
+		KV_URL: z.string().url(),
 	},
 
 	/**
@@ -25,6 +29,10 @@ export const env = createEnv({
 	 */
 	runtimeEnv: {
 		NODE_ENV: process.env.NODE_ENV,
+		KV_REST_API_READ_ONLY_TOKEN: process.env.KV_REST_API_READ_ONLY_TOKEN,
+		KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
+		KV_REST_API_URL: process.env.KV_REST_API_URL,
+		KV_URL: process.env.KV_URL,
 		// NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 	},
 	/**
